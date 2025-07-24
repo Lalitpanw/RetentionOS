2ce934dc1fbe46e294f6ae68d8e651a2327a8a73
Lalitpanw <153562215+Lalitpanw@users.noreply.github.com>
2025-07-24 23:53:32 +0530

    app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.set\_page\_config(page\_title="RetentionOS", layout="wide")
+st.set_page_config(page_title="RetentionOS", layout="wide")
 
 # Sidebar menu without About
 
